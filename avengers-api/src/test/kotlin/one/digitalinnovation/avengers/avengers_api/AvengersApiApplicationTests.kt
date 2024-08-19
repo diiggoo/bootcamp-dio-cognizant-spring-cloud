da1b71dfa752f20c312a5f4e21ef363d2839da93
Rodrigo Moreira <rodrigo.apvm@gmail.com>
2024-08-19 11:56:33 -0300

    create avengers-api first version

---
+++ avengers-api/src/test/kotlin/one/digitalinnovation/avengers/avengers_api/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.avengers.avengers_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
